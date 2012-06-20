0e73aa5f26ac19539c5d79d156a50a2d6ecd4dfe
slieser <stefan@lieser-online.de>
2012-06-20 20:00:35 +0200

    GUI added

---
+++ az.gui/az.gui.tests/MainWindowTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using NUnit.Framework;
+
+namespace az.gui.tests
+{
+    [TestFixture]
+    public class MainWindowTests
+    {
+        [Test, Explicit, RequiresSTA]
+        public void Event_wird_ausgelöst() {
+            var mainWindow = new MainWindow();
+            mainWindow.Versenden += s => MessageBox.Show(s);
+
+            mainWindow.ShowDialog();
+        }
+
+        [Test, Explicit, RequiresSTA]
+        public void Status_wird_gesetzt() {
+            var mainWindow = new MainWindow();
+            mainWindow.Versandstatus("Success!");
+
+            mainWindow.ShowDialog();
+        }
+    }
+}
---
+++ az.gui/az.gui/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace az.gui
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+
+            btnSenden.Click += (o, e) => Versenden(txtTweetText.Text);
+        }
+
+        public event Action<string> Versenden;
+
+        public void Versandstatus(string message) {
+            lblStatus.Text = message;
+        }
+    }
+}
