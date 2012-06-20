ca39337f9905718ee02d882a8a4858a7ea3dcd64
Ralf Westphal <info@ralfw.de>
2012-06-20 14:14:00 +0200

    kleiner spike, um den workflow für die app autorisierung bei twitter zu testen.

---
+++ spikes/WorkflowWithBrowser/WorkflowWithBrowser/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkflowWithBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var p = new Process
+                        {
+                            StartInfo = new ProcessStartInfo("iexplore.exe")
+                                            {
+                                                Arguments = "http://www.clean-code-advisors.de",
+                                                WindowStyle = ProcessWindowStyle.Maximized
+                                            }
+                        };
+            p.Start();
+            p.WaitForExit();
+            p.Close();
+
+            textBox1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
