35795fe107f376dbcab2cca2db57f02f7c8c3a7b
Ralf Westphal <info@ralfw.de>
2012-06-20 21:02:35 +0200

    1. slide fertig: client twittert direkt mit fixem access token.

---
+++ source/az.application/az.application/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using az.gui;
+using npantarhei.runtime;
+using npantarhei.runtime.patterns;
+
+namespace az.application
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var twitterops = new TwitterOperations();
+            var gui = new MainWindow();
+
+            FlowRuntimeConfiguration.SynchronizationFactory = () => new SyncWithWPFDispatcher();
+
+            var config = new FlowRuntimeConfiguration()
+                .AddStreamsFrom("az.application.flows.flow", Assembly.GetExecutingAssembly())
+
+                .AddFunc<string, Versandauftrag>("versandauftrag_schnueren", twitterops.Versandauftrag_schnüren)
+                .AddFunc<Versandauftrag, string>("versenden", twitterops.Versenden)
+                .AddAction<string>("versandstatus_anzeigen", gui.Versandstatus).MakeSync();
+
+            using (var fr = new FlowRuntime(config))
+            {
+                fr.UnhandledException += ex => MessageBox.Show(ex.InnerException.Message);
+
+                gui.Versenden += fr.CreateEventProcessor<string>(".versenden");
+
+                var app = new System.Windows.Application {MainWindow = gui};
+                app.Run(gui);
+            }
+        }
+    }
+}
