a1d82b41792cd4d7a1e5026ac7f3c339f0359430
Ralf Westphal <info@ralfw.de>
2012-06-20 19:29:46 +0200

    verzeichnisstruktur angelegt. lib gefüllt.

---
+++ source/az.application/az.twitter/Twitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace az.twitter
+{
+    public class Versandauftrag
+    {
+        public int I { get; private set; }
+    }
+
+    public class Twitter
+    {
+        public Versandauftrag Versandauftrag_schnüren(string text)
+        {
+            
+        }
+    }
+}
