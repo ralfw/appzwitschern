48091df7bce056176bb3280c3f0756fed02e4246
slieser <stefan@lieser-online.de>
2012-06-21 16:07:32 +0200

    root.flow embedded as resource

--- source/az.receiver.application/az.receiver.application/Program.cs
+++ source/az.receiver.application/az.receiver.application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using az.security;
 using az.serialization;
 using az.sqsapi;
@@ -12,11 +13,14 @@ namespace az.receiver.application
         private static void Main(string[] args) {
             var frc = new FlowRuntimeConfiguration();
             frc.AddStreamsFrom("az.receiver.application.root.flow", Assembly.GetExecutingAssembly());
-            frc.AddOperation(new SQSDequeue("dequeue", "appzwitschern", TokenRepository.LoadFrom("aws.credentials.txt")));
+            frc.AddOperation(new SQSDequeue("dequeue", "AppZwitschern", TokenRepository.LoadFrom("aws.credentials.txt")));
             frc.AddFunc<string, Versandauftrag>("deserialize", new Serialization<Versandauftrag>().Deserialize);
             frc.AddFunc<Versandauftrag, string>("versenden", new TwitterOperations().Versenden);
             
             using(var fr = new FlowRuntime(frc)) {
+                fr.Message += Console.WriteLine;
+                fr.UnhandledException += e => Console.WriteLine(e.InnerException.Message);
+
                 fr.Process(".start");
                 fr.WaitForResult();
             }
