098ed0b927dfaff514fc6895139499cc549ac011
slieser <stefan@lieser-online.de>
2012-06-21 20:42:36 +0200

    az.application angepasst an neue GUI

--- source/az.application/az.application/Program.cs
+++ source/az.application/az.application/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows;
+using az.contracts;
 using az.gui;
 using az.security;
+using az.serialization;
+using az.sqsapi;
 using az.twitterapi;
 using npantarhei.runtime;
 using npantarhei.runtime.patterns;
@@ -15,30 +15,27 @@ namespace az.application
     public class Program
     {
         [STAThread]
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             var twitterops = new TwitterOperations();
             var gui = new MainWindow();
-            var enqueue = new az.sqsapi.SQSEnqueue("enqueue", "AppZwitschern", TokenRepository.LoadFrom("aws.credentials.txt"));
-            var serialisieren = new az.serialization.Serialization<Versandauftrag>();
+            var sqs = new SQSOperations("AppZwitschern", TokenRepository.LoadFrom("aws.credentials.txt"));
+            var serialisieren = new Serialization<Versandauftrag>();
 
             FlowRuntimeConfiguration.SynchronizationFactory = () => new SyncWithWPFDispatcher();
 
             var config = new FlowRuntimeConfiguration()
                 .AddStreamsFrom("az.application.flows.flow", Assembly.GetExecutingAssembly())
-
-                .AddFunc<string, Versandauftrag>("versandauftrag_schnueren", twitterops.Versandauftrag_schnüren)
+                .AddFunc<Versandauftrag, Versandauftrag>("versandauftrag_schnueren", twitterops.Versandauftrag_um_access_token_erweitern)
                 .AddFunc<Versandauftrag, string>("serialisieren", serialisieren.Serialize)
-                .AddOperation(enqueue)
+                .AddAction<string>("enqueue", sqs.Enqueue)
                 .AddAction("versandstatus_anzeigen", () => gui.Versandstatus("Versendet!")).MakeSync();
 
-            using (var fr = new FlowRuntime(config))
-            {
+            using (var fr = new FlowRuntime(config)) {
                 fr.UnhandledException += ex => MessageBox.Show(ex.InnerException.Message);
 
-                gui.Versenden += fr.CreateEventProcessor<string>(".versenden");
+                gui.Versenden += fr.CreateEventProcessor<Versandauftrag>(".versenden");
 
-                var app = new Application {MainWindow = gui};
+                var app = new Application { MainWindow = gui };
                 app.Run(gui);
             }
         }
---
+++ source/az.publisher.application/az.publisher.application/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using az.contracts;
+using az.twitterapi;
+using npantarhei.runtime;
+
+namespace az.publisher.application
+{
+    internal class Program
+    {
+        private static void Main(string[] args) {
+            var frc = new FlowRuntimeConfiguration();
+            frc.AddStreamsFrom("az.publisher.application.root.flow", Assembly.GetExecutingAssembly());
+            frc.AddAction<Versandauftrag>("load");
+            frc.AddFunc<Versandauftrag, Versandauftrag>("filtern", _ => _);
+            frc.AddFunc<Versandauftrag, string>("versenden", new TwitterOperations().Versenden);
+            frc.AddAction<string>("delete");
+
+            using(var fr = new FlowRuntime(frc)) {
+                fr.UnhandledException += e => Console.WriteLine(e.InnerException.Message);
+
+                fr.Process(".start");
+                fr.WaitForResult();
+            }
+        }
+    }
+}
