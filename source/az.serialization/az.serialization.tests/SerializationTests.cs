78650cc8c9a0023e1d0e8974972710e4263abe2c
slieser <stefan@lieser-online.de>
2012-06-21 21:26:54 +0200

    Deserialize läßt null durch

--- source/az.serialization/az.serialization.tests/SerializationTests.cs
+++ source/az.serialization/az.serialization.tests/SerializationTests.cs
@@ -5,14 +5,25 @@ namespace az.serialization.tests
     [TestFixture]
     public class SerializationTests
     {
+        private Serialization<Person> sut;
+
+        [SetUp]
+        public void Setup() {
+            sut = new Serialization<Person>();
+        }
+
         [Test]
         public void Zusammengesetzter_Typ() {
-            var sut = new Serialization<Person>();
             var serialized = sut.Serialize(new Person { Name = "Peter", Alter = 42 });
 
             var person = sut.Deserialize(serialized);
             Assert.That(person.Name, Is.EqualTo("Peter"));
             Assert.That(person.Alter, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void Null_wird_durchgereicht_zur_end_of_stream_Erkennung() {
+            Assert.That(sut.Deserialize(null), Is.Null);
         }
 
         private class Person
